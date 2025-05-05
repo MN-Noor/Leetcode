cfd25fc327b40f47b3e107cc15a47c1ae2db7533
Aymen Noor <108609236+MN-Noor@users.noreply.github.com>
2025-05-05 09:44:36 +0500

    Time: 81 ms (77.5%), Space: 28 MB (25.56%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -30,25 +30,36 @@
 #                 return search
 #             search=search.next
 #         return None
+#Difference Approach
+# class Solution:
+#     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+#         sizeA=self.find_length(headA)
+#         sizeB=self.find_length(headB)
+#         longer=headA if sizeA>sizeB else headB
+#         smaller=headA if sizeA<=sizeB else headB
+#         diff=abs(sizeA-sizeB)
+#         for i in range(diff):
+#             longer=longer.next
+#         while smaller!=None:
+#             if smaller==longer:
+#                 return smaller
+#             else:
+#                 smaller=smaller.next
+#                 longer=longer.next       
+#     def find_length(self,node):
+#         size=0
+#         while node!=None:
+#             node=node.next
+#             size+=1
+#         return size
 
+#swappingList Approach
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        sizeA=self.find_length(headA)
-        sizeB=self.find_length(headB)
-        longer=headA if sizeA>sizeB else headB
-        smaller=headA if sizeA<=sizeB else headB
-        diff=abs(sizeA-sizeB)
-        for i in range(diff):
-            longer=longer.next
-        while smaller!=None:
-            if smaller==longer:
-                return smaller
-            else:
-                smaller=smaller.next
-                longer=longer.next       
-    def find_length(self,node):
-        size=0
-        while node!=None:
-            node=node.next
-            size+=1
-        return size
+     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        iterA=headA
+        iterB=headB
+        while iterA!=iterB:
+            iterA=iterA.next if iterA else headB
+            iterB=iterB.next if iterB else headA            
+        return iterA
+

a05b4a1e95b17318c5926d5c9d9bd3f52c7bcd1f
mn <108609236+MN-NR@users.noreply.github.com>
2026-08-18 16:50:14 -0700

    Time: 0 ms (100%), Space: 19.4 MB (68.27%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def dfs(index,arr):
-            results.append(arr) 
-            if index>=len(nums):
-                return  
-            for i in range(index,len(nums)):                         
-                dfs(i+1,arr+[nums[i]])  
-             
-        results=[]
-        dfs(0,[])            
-        return results
+        self.powerset=[]
+        def dfs(i,arr):
+            if i>=len(nums):
+                self.powerset.append(arr)
+                return
+            else:
+                dfs(i+1,arr+[nums[i]])
+                dfs(i+1,arr)
+        arr=[]
+        dfs(0,arr)
+        return self.powerset
+        
